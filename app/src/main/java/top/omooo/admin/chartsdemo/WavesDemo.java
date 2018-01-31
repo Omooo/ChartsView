e63e16c225c706b02c68d090d5efebcf64bb6950
Omooo <869759698@qq.com>
2018-01-31 19:31:21 +0800

    common commit

--- app/src/main/java/top/omooo/admin/chartsdemo/WavesDemo.java
+++ app/src/main/java/top/omooo/admin/chartsdemo/WavesDemo.java
@@ -4,16 +4,14 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
-import top.omooo.admin.chartsdemo.view.WavesView;
-
 /**
  * Created by Omooo on 2018/1/26.
  */
 
-public class PracticeActivity extends AppCompatActivity {
+public class WavesDemo extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_practice);
+        setContentView(R.layout.activity_waves);
     }
 }
---
+++ app/src/main/java/top/omooo/admin/chartsdemo/view/NativeEditView.java
@@ -0,0 +1,24 @@
+package top.omooo.admin.chartsdemo.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
+/**
+ * Created by Omooo on 2018/1/31.
+ */
+
+public class NativeEditView extends android.support.v7.widget.AppCompatEditText {
+
+    public NativeEditView(Context context) {
+        super(context);
+    }
+
+    public NativeEditView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public NativeEditView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
