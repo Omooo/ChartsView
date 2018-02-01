ccacbc6a09e51d36f70d70ae94a3976f9b28d5a2
Omooo <869759698@qq.com>
2018-02-01 17:15:59 +0800

    自定义一键删除的EditText

---
+++ app/src/main/java/top/omooo/admin/chartsdemo/NativeEditDemo.java
@@ -0,0 +1,17 @@
+package top.omooo.admin.chartsdemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Omooo on 2018/2/1.
+ */
+
+public class NativeEditDemo extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_native_edit);
+    }
+}
--- app/src/main/java/top/omooo/admin/chartsdemo/view/NativeEditView.java
+++ app/src/main/java/top/omooo/admin/chartsdemo/view/NativeEditView.java
@@ -1,24 +1,117 @@
 package top.omooo.admin.chartsdemo.view;
 
+import android.animation.AnimatorSet;
+import android.animation.ValueAnimator;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.RotateDrawable;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.EditText;
+
+import top.omooo.admin.chartsdemo.R;
 
 /**
  * Created by Omooo on 2018/1/31.
  */
 
-public class NativeEditView extends android.support.v7.widget.AppCompatEditText {
+public class NativeEditView extends android.support.v7.widget.AppCompatEditText implements TextWatcher{
 
+    private RotateDrawable mRotateDrawable;
+    private ValueAnimator alphaAnimator = ValueAnimator.ofInt(0, 255);
+    private ValueAnimator rotateAnimator = ValueAnimator.ofInt(0, 10000);
+
+    private void init() {
+        setIconVisiable(false,getCompoundDrawables());
+    }
+
+    private void setIconVisiable(boolean b, Drawable[] drawables) {
+        if (b) {
+            setCompoundDrawablesWithIntrinsicBounds(drawables[0], drawables[1], getResources().getDrawable(R.drawable.edit_icon), drawables[3]);
+            mRotateDrawable = (RotateDrawable) getCompoundDrawables()[2];
+        } else {
+            setCompoundDrawablesWithIntrinsicBounds(drawables[0], drawables[1], null, drawables[3]);
+        }
+    }
     public NativeEditView(Context context) {
         super(context);
+        init();
     }
 
     public NativeEditView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public NativeEditView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
     }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        if (s.length() == 0 && before > 0) {
+            //从有文字到删除文字时
+            return;
+        }
+        if (start == 0 && s.length() > 0) {
+            //从无文字到有文字
+            setIconVisiable(true, getCompoundDrawables());
+            setAnimator();
+            startAnimatorSet();
+        }
+    }
+    //启动动画
+    private void startAnimatorSet() {
+        AnimatorSet setVisible = new AnimatorSet();
+        setVisible.playTogether(alphaAnimator, rotateAnimator);
+        setVisible.start();
+    }
+    private void setAnimator() {
+        alphaAnimator.setDuration(1000);
+        alphaAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+        alphaAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                mRotateDrawable.setAlpha((Integer) animation.getAnimatedValue());
+            }
+        });
+
+        rotateAnimator.setDuration(1000);
+        rotateAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+        rotateAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                mRotateDrawable.setLevel((Integer) animation.getAnimatedValue());
+            }
+        });
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_UP:
+                if (getCompoundDrawables()[2] != null) {
+                    if (getWidth() - getTotalPaddingRight() < event.getX() && getWidth() - getPaddingRight() > event.getX()) {
+                        this.setText("");
+                    }
+                }
+                break;
+        }
+        return super.onTouchEvent(event);
+    }
+
 }
