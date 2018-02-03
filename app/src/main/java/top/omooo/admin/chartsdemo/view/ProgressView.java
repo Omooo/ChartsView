b9e0729c17f0742451db2529569f59ec306524e5
Omooo <869759698@qq.com>
2018-02-03 21:12:30 +0800

    common commit

---
+++ app/src/main/java/top/omooo/admin/chartsdemo/view/ProgressView.java
@@ -0,0 +1,27 @@
+package top.omooo.admin.chartsdemo.view;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Omooo on 2018/2/3.
+ */
+
+/**
+ * 进度条
+ */
+public class ProgressView extends View {
+    public ProgressView(Context context) {
+        super(context);
+    }
+
+    public ProgressView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ProgressView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
